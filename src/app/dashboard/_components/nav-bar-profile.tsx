dd1ebdfeecbc6bfded7ac896a5e7856a07fce0c8
Rob van Bakel <robbakel@gmail.com>
2024-12-11 12:22:40 +0000

    Show display name on dashboard

--- src/app/dashboard/_components/nav-bar-profile.tsx
+++ src/app/dashboard/_components/nav-bar-profile.tsx
@@ -1,9 +1,20 @@
+import { db } from "@/lib/drizzle/db";
+import { UsersTable } from "@/lib/drizzle/schema";
 import { UserButton } from "@clerk/nextjs";
 import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
 
 export default async function NavBarProfile() {
-  const user = await currentUser();
+  const auth = await currentUser();
+
+  if (!auth) {
+    redirect("/");
+  }
+
+  const user = await db.query.UsersTable.findFirst({
+    where: eq(UsersTable.externalId, auth.id),
+  });
 
   if (!user) {
     redirect("/");
@@ -12,9 +23,9 @@ export default async function NavBarProfile() {
   return (
     <div className="flex h-10 items-center gap-4 md:ml-auto">
       <div className="hidden flex-col text-right md:flex">
-        <span className="text-sm font-semibold">{user.fullName}</span>
+        <span className="text-sm font-semibold">{user.displayName}</span>
         <span className="text-xs text-muted-foreground">
-          {user.emailAddresses[0].emailAddress}
+          {auth.emailAddresses[0].emailAddress}
         </span>
       </div>
       <UserButton
