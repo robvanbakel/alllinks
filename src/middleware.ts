9825a2d0a2c3399affac218eb9d66582df4b4767
Rob van Bakel <robbakel@gmail.com>
2024-12-22 20:13:21 +0000

    Add middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,3 +6,7 @@ export default auth((req) => {
   const newUrl = new URL("/dashboard/links", req.nextUrl.origin);
   return Response.redirect(newUrl);
 });
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
